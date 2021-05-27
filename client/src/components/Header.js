10a81ea5fb7879c91ce00076cb40cf48b941c4e2
omar-dahdouh <omar.m.dahdouh@gmail.com>
2021-05-27 15:11:55 +0300

    home page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,37 +1,13 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { Layout, Input } from 'antd';
 
 const Header = () => {
-  const [searchInput, setSearchInput] = useState('');
-
-  const history = useHistory();
-  function onSearchInputChange({ target }) {
-    // console.log(target.value);
-    setSearchInput(target.value);
-  }
-
-  function onSearch() {
-    // console.log(searchInput);
-    history.push(`/search/${searchInput}`);
-  }
-
   return (
     <Layout.Header>
-      <Input.Search
-        value={searchInput}
-        onSearch={onSearch}
-        onChange={onSearchInputChange}
-        style={{ width: 400, padding: 16 }}
-        placeholder="input search text"
-        enterButton
-      />
-      {/* <div className="logo" /> */}
-      {/* <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
-      </Menu> */}
+      <Link to="/" className="logo">
+        Course Search
+      </Link>
     </Layout.Header>
   );
 };
