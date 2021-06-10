3c2601d664a007fb4cbb5a5fe306316748223dd5
omar-dahdouh <omar.m.dahdouh@gmail.com>
2021-06-10 11:10:36 +0300

    fix connection

--- server/src/database/config/connection.js
+++ server/src/database/config/connection.js
@@ -21,7 +21,7 @@ switch (process.env.NODE_ENV) {
 
 const options = {
   connectionString: dbUrl,
-  ssl: process.env.NODE_ENV === 'production',
+  ssl: { rejectUnauthorized: false },
 };
 
 module.exports = new Pool(options);
